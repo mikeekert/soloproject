6165b83fa8c3f42a50719169f6ba20306963eee3
Mike Ekert <mikeekert@gmail.com>
2017-10-28 12:56:10 -0500

    fix login

---
+++ server/public/scripts/controllers/about.controller.js
@@ -0,0 +1,6 @@
+myApp
+    .controller('AboutController', function ($http, $location, UserService) {
+        var vm = this;
+        vm.userService = UserService;
+        vm.userObject = UserService.userObject;
+    });
--- server/public/scripts/controllers/user.controller.js
+++ server/public/scripts/controllers/user.controller.js
@@ -1,13 +1,16 @@
-myApp.controller('UserController', function(UserService) {
-  var vm = this;
-  vm.userService = UserService;
-  vm.userObject = UserService.userObject;
-  vm.Math = window.Math;
+myApp
+  .controller('UserController', function (UserService) {
+    var vm = this;
+    vm.userService = UserService;
+    vm.userObject = UserService.userObject;
+    vm.Math = window.Math;
 
-  vm.userService.getgames();
-  
-vm.inProgress = function (filter) {
-  return filter.nowplaying === false || filter.completed === false; 
-};
+    vm
+      .userService
+      .getgames();
 
-});
+    vm.inProgress = function (filter) {
+      return filter.nowplaying === false || filter.completed === false;
+    };
+
+  });
