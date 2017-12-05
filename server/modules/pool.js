8ca757382d87ad08f83699bdb029e7cf96e21075
Mike Ekert <mikeekert@gmail.com>
2017-12-05 10:26:32 -0600

    aws DB info

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -26,12 +26,16 @@ if (process.env.DATABASE_URL) {
   };
 
 } else {
+  // host     : process.env.RDS_HOSTNAME,
+  // user     : process.env.RDS_USERNAME,
+  // password : process.env.RDS_PASSWORD,
+  // port     : process.env.RDS_PORT
   config = {
-    user: process.env.PG_USER || null, //env var: PGUSER
-    password: process.env.DATABASE_SECRET || null, //env var: PGPASSWORD
-    host: process.env.DATABASE_SERVER || 'localhost', // Server hosting the postgres database
-    port: process.env.DATABASE_PORT || 5432, //env var: PGPORT
-    database: process.env.DATABASE_NAME || 'Questlog', //env var: PGDATABASE
+    user: process.env.RDS_USERNAME || null, //env var: PGUSER
+    password: process.env.RDS_PASSWORD || null, //env var: PGPASSWORD
+    host: process.env.RDS_HOSTNAME || 'localhost', // Server hosting the postgres database
+    port: process.env.RDS_PORT || 5432, //env var: PGPORT
+    database: process.env.RDS_DB_NAME || 'Questlog', //env var: PGDATABASE
     max: 10, // max number of clients in the pool
     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
   };
